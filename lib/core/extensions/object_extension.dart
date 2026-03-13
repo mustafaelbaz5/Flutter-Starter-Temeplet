f6b2e441dc6d4e34a36044b536e2fa2892a6729e
mustafaelbaz5 <m9stafa05@gmail.com>
2026-03-13 09:42:05 +0200

    Update and NEw extensions

--- lib/core/extensions/object_extension.dart
+++
@@ -1,8 +0,0 @@
-import '../error/models/app_error.dart';
-import '../error/types/error_handler.dart';
-
-extension ErrorHandlerExtension on Object {
-  AppError toAppError() {
-    return ErrorHandler.handle(this);
-  }
-}
---
+++ lib/core/utils/extensions/extensions.dart
@@ -0,0 +1,5 @@
+export 'string_ext.dart';
+export 'num_ext.dart';
+export 'list_ext.dart';
+export 'datetime_ext.dart';
+export 'context_ext.dart';
