8d3b506b64e66a879415a996f4ae303e700fb6de
mustafaelbaz5 <m9stafa05@gmail.com>
2026-03-13 12:00:39 +0200

    Build Test Screen

--- lib/features/feature_ex/ui/app_screen.dart
+++ lib/features/feature_ex/ui/app_screen.dart
@@ -1,7 +1,9 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-
-import '../../../core/utils/functions/app_setting_method.dart';
+import 'package:flutter_structure/core/utils/extensions/num_ext.dart';
+import 'package:flutter_structure/core/utils/functions/app_setting_method.dart';
+import 'package:flutter_structure/core/utils/spacing.dart';
+import 'package:flutter_structure/core/widgets/custom_text_button.dart';
+import 'package:flutter_structure/core/widgets/custom_text_form_.dart';
 
 class AppScreen extends StatelessWidget {
   const AppScreen({super.key});
@@ -9,32 +11,49 @@ class AppScreen extends StatelessWidget {
   @override
   Widget build(final BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("app_title".tr())),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text("Hi from Theme mode"),
-            const SizedBox(height: 32),
-            const Text("ازيكم يا جماعة"),
-            const SizedBox(height: 32),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  switchTheme(context);
-                },
-                child: const Text("Switch Theme"),
+        padding: EdgeInsets.all(rr(16)),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(5.toCurrency()),
+              SizedBox(height: rh(32)),
+              const Text("ازيكم يا جماعة"),
+              SizedBox(height: rh(32)),
+              const CustomTextForm(
+                hintText: 'Email',
+                isPassword: true,
+                textInputAction: TextInputAction.done,
               ),
-            ),
-            const SizedBox(height: 32),
-            Center(
-              child: ElevatedButton(
-                onPressed: () => switchLanguage(context),
-                child: const Text("Switch Language"),
+              verticalSpacing(56),
+              const CustomTextForm(
+                hintText: 'password',
+                isPassword: true,
+                keyboardType: TextInputType.number,
               ),
-            ),
-          ],
+              verticalSpacing(56),
+              Center(
+                child: CustomTextButton(
+                  isFullWidth: false,
+                  text: "Swith Theme",
+                  onPressed: () {
+                    switchTheme(context);
+                  },
+                ),
+              ),
+              verticalSpacing(32),
+              Center(
+                child: CustomTextButton.outlined(
+                  isFullWidth: false,
+                  text: "Switch lan",
+                  onPressed: () {
+                    switchLanguage(context);
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
