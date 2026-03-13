0d273b7d3d8f67e8d4b7e4aa3a54540d795bbbd6
mustafaelbaz5 <m9stafa05@gmail.com>
2026-03-13 12:20:10 +0200

    Update TExting Screen

--- lib/features/feature_ex/ui/app_screen.dart
+++ lib/features/feature_ex/ui/app_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_structure/core/themes/app_text_styles.dart';
+import 'package:flutter_structure/core/utils/extensions/context_ext.dart';
+
 import '../../../core/utils/extensions/num_ext.dart';
 import '../../../core/utils/functions/app_setting_method.dart';
 import '../../../core/utils/spacing.dart';
@@ -11,48 +14,62 @@ class AppScreen extends StatelessWidget {
   @override
   Widget build(final BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(rr(16)),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(5.toCurrency()),
-              SizedBox(height: rh(32)),
-              const Text("ازيكم يا جماعة"),
-              SizedBox(height: rh(32)),
-              const CustomTextForm(
-                hintText: 'Email',
-                isPassword: true,
-                textInputAction: TextInputAction.done,
-              ),
-              verticalSpacing(56),
-              const CustomTextForm(
-                hintText: 'password',
-                isPassword: true,
-                keyboardType: TextInputType.number,
-              ),
-              verticalSpacing(56),
-              Center(
-                child: CustomTextButton(
-                  isFullWidth: false,
-                  text: "Swith Theme",
-                  onPressed: () {
-                    switchTheme(context);
-                  },
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(rr(16)),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text("Hello World", style: AppTextStyles.font20SemiBold),
+                Text(5.toCurrency()),
+                verticalSpacing(32),
+                Text(
+                  "السلام عليكم ورحمة",
+                  style: AppTextStyles.font18Light.copyWith(
+                    color: context.customColors.info,
+                  ),
                 ),
-              ),
-              verticalSpacing(32),
-              Center(
-                child: CustomTextButton.outlined(
-                  isFullWidth: false,
-                  text: "Switch lan",
-                  onPressed: () {
-                    switchLanguage(context);
-                  },
+                Text(
+                  "صلي على نبينا محمد",
+                  style: AppTextStyles.font18Light.copyWith(
+                    color: context.customColors.error,
+                  ),
                 ),
-              ),
-            ],
+                SizedBox(height: rh(32)),
+                const CustomTextForm(
+                  hintText: 'Email',
+                  isPassword: true,
+                  textInputAction: TextInputAction.done,
+                ),
+                verticalSpacing(56),
+                const CustomTextForm(
+                  hintText: 'password',
+                  isPassword: true,
+                  keyboardType: TextInputType.number,
+                ),
+                verticalSpacing(56),
+                Center(
+                  child: CustomTextButton(
+                    isFullWidth: false,
+                    text: "Swith Theme",
+                    onPressed: () {
+                      switchTheme(context);
+                    },
+                  ),
+                ),
+                verticalSpacing(32),
+                Center(
+                  child: CustomTextButton.outlined(
+                    isFullWidth: false,
+                    text: "Switch lan",
+                    onPressed: () {
+                      switchLanguage(context);
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
