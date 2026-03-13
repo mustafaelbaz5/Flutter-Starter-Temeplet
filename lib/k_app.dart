c23067bf285fe27f0454495c4eb6503df5b05bc0
mustafaelbaz5 <m9stafa05@gmail.com>
2026-03-13 12:00:22 +0200

    Update App Init

--- lib/k_app.dart
+++ lib/k_app.dart
@@ -2,16 +2,15 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'core/router/app_router.dart';
+import 'core/router/routes.dart';
 import 'core/themes/cubit/theme_cubit.dart';
 import 'core/themes/theme_data/theme_data_dark.dart';
 import 'core/themes/theme_data/theme_data_light.dart';
 
-import 'core/router/app_router.dart';
-import 'core/router/routes.dart';
-
 class KApp extends StatelessWidget {
-  const KApp({super.key, required this.appRouter});
-  final AppRouter appRouter;
+  const KApp({super.key});
   @override
   Widget build(final BuildContext context) {
     return ScreenUtilInit(
@@ -21,17 +20,15 @@ class KApp extends StatelessWidget {
       builder: (final BuildContext context, final Widget? child) {
         return BlocProvider(
           create: (final BuildContext context) => ThemeCubit(),
-
           child: BlocBuilder<ThemeCubit, ThemeMode>(
             builder: (final BuildContext context, final ThemeMode mode) {
               return MaterialApp(
-                key: ValueKey(context.locale),
                 localizationsDelegates: context.localizationDelegates,
                 supportedLocales: context.supportedLocales,
                 locale: context.locale,
                 debugShowCheckedModeBanner: false,
                 initialRoute: Routes.appScreen,
-                onGenerateRoute: appRouter.generateRoute,
+                onGenerateRoute: AppRouter.generateRoute,
                 title: 'App Title',
                 theme: getLightTheme(context: context),
                 darkTheme: getDarkTheme(context: context),
--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,33 +1,40 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_structure/core/localization/localization_manager.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 
 import 'core/di/dependency_injection.dart';
-import 'core/router/app_router.dart';
 import 'k_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+  // Set preferred orientations
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: kIsWeb
         ? HydratedStorageDirectory.web
         : HydratedStorageDirectory((await getTemporaryDirectory()).path),
   );
-  await setupGetIt();
+
+  await setupDependencies();
   await ScreenUtil.ensureScreenSize();
 
   runApp(
     EasyLocalization(
-      supportedLocales: const [Locale('en'), Locale('ar')],
-      path: 'assets/translations',
-      startLocale: const Locale('en'),
-      fallbackLocale: const Locale('en'),
-      child: KApp(appRouter: AppRouter()),
+      supportedLocales: LocalizationManager.supportedLocales,
+      path: LocalizationManager.translationsPath,
+      fallbackLocale: LocalizationManager.fallbackLocale,
+      startLocale: LocalizationManager.fallbackLocale,
+      child: const KApp(),
     ),
   );
 }
